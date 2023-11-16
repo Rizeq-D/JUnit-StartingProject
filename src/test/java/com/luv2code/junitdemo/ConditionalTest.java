f0d803b7e48eaf23f91473f4e3a51590b7a77009
Abdulrazak <rizeq@Abdulrazaks-MacBook-Pro.local>
2023-11-16 16:01:52 +0100

    added new tests for Mac, Windows, and Linux

---
+++ src/test/java/com/luv2code/junitdemo/ConditionalTest.java
@@ -0,0 +1,69 @@
+package com.luv2code.junitdemo;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConditionalTest {
+
+    @Test
+    @Disabled("Do not run this test until JIRA #123 is resolved")
+    void basicTest() {
+        // execute method and perform asserts
+    }
+
+    @Test
+    @EnabledOnOs(OS.WINDOWS)
+    void testForWindowsOnly() {
+        // execute method and perform asserts
+    }
+
+    @Test
+    @EnabledOnOs(OS.MAC)
+    void testForMacOnly() {
+        // execute method and perform asserts
+    }
+
+    @Test
+    @EnabledOnOs({OS.MAC, OS.WINDOWS})
+    void testForMacAndWindowsOnly() {
+        // execute method and perform asserts
+    }
+
+    @Test
+    @EnabledOnOs(OS.LINUX)
+    void testForLinuxOnly() {
+        // execute method and perform asserts
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
